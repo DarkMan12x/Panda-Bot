64cb5ad90abcb93da81d4f0ae269b3325da51155
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-07-04 01:05:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -835,7 +835,7 @@ client.on(`ready`, () => {
       });
       
     client.user.setActivity(`Type: ${PREFIX}help  - Apple Bot`, { type: "PLAYING"});
-    client.user.setActivity(`Type: ${PREFIX}help - $help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
+    client.user.setActivity(`Type: ${PREFIX}help - >help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
    
   
       }, (5000));
